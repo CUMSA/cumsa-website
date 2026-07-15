41633a6b64d59a9d7b4034fbbacd8821a57a0787
EthanKuai <ethankuaiek@gmail.com>
2026-07-16 02:38:51 +0800

    fix: footer icons alignment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <>
       <SponsorCarousel />
       <footer className="bg-slate-950 text-white py-6">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-start">
 
             <div className="mb-8 md:mb-0 flex">
@@ -22,27 +22,27 @@ export default function Footer() {
                 />
               </div>
 
-              <div className="items-center justify-center mx-4">
-                <h3>
+              <div className="mx-1">
+                <h6>
                   Cambridge University Malaysia
-                </h3>
-                <h3>
+                </h6>
+                <h6>
                   and Singapore Association
-                </h3>
-              </div>
-
-              <div className="flex items-center justify-center space-x-2 gap-4 ml-5">
-                {
-                  socials.map((s: Social) => (
-                    <a key={s.name} href={s.href} title={s.name} target="_blank" rel="external" className="p-2 text-white hover:text-gray-300 transition-colors">
-                      {s.icon}
-                    </a>
-                  ))
-                }
+                </h6>
               </div>
             </div>
 
-            <div className=" text-center">
+            <div className="flex items-center space-x-2 gap-4">
+              {
+                socials.map((s: Social) => (
+                  <a key={s.name} href={s.href} title={s.name} target="_blank" rel="external" className="p-2 text-white hover:text-gray-300 transition-colors">
+                    {s.icon}
+                  </a>
+                ))
+              }
+            </div>
+
+            <div className="ml-36 text-center">
               <div className="text-sm text-gray-400">
                 <p>© Copyright 2025 CUMSA.</p>
                 <p>All rights reserved</p>
